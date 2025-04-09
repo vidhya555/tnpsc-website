2405acb3cf9e4428122c5ba01b668f5b5df94708
vidhya555 <chithraponraj333@gmail.com>
2025-04-09 14:17:10 +0530

    Create App.jsx

---
+++ public/src/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Welcome to Chithu's TNPSC & Shorthand Notes 📝</h1>
+      <p>More content coming soon... 🚀</p>
+    </div>
+  );
+}
+
+export default App;
